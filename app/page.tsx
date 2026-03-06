9ebbedea4f32855e44174cd91ab49113f4926ac0
siripong-ctn <siripong.2320@gmail.com>
2026-03-06 22:31:53 +0700

    feat: enhance Home component with post image display and reference links

--- app/page.tsx
+++ app/page.tsx
@@ -33,58 +33,67 @@ export default function Home() {
 
         {/* Header */}
         <div className="flex items-center gap-4 mb-12">
-          <Image
-            src="/next.svg"
-            alt="Next.js logo"
-            width={40}
-            height={40}
-            className="invert"
-          />
-
-          <div>
             <h1 className="text-3xl font-bold">Personal Portfolio with Blog</h1>
             <p className="text-zinc-400 text-sm">
               Create a personal portfolio website with 
               an integrated blog where users can add, edit, and delete posts.
               This is an excellent project for learning the basics of frontend
               and backend development and gaining foundational experience in React and Next.js.
-              <br></br>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                <a href="#">Go create</a>
-              </button>
+              <br /><br />
+              <a href="#" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Go create
+              </a>
             </p>
-          </div>
         </div>
 
         {/* Posts */}
         <div className="space-y-6">
-
-          {posts.length === 0 && (
-            <p className="text-zinc-500">No posts found</p>
-          )}
 
           {posts.map((post) => (
             <div
               key={post.id}
               className="bg-zinc-900 border border-zinc-800 rounded-xl p-6 hover:border-zinc-600 transition"
             >
+
               <h2 className="text-xl font-semibold mb-2">
                 {post.title || "Untitled"}
               </h2>
 
-              <p className="text-zinc-300 mb-2">
+              {post.image && (
+                <Image
+                  src={`/images/${post.image}`}
+                  alt={post.title || "Post image"}
+                  width={1920}
+                  height={1080}
+                  className="rounded-lg mb-4 object-cover w-full"
+                />
+              )}
+              
+              <p className="text-zinc-300 mb-2 text-sm">
                 {post.content}
+                <br/>
+              </p>
+
+              <p className="mb-2 text-sm">
+                Reference:
+              </p>
+
+              <p className="mb-5 text-xs">
+                <a href={post.about1} target="_blank" className="bg-slate-500 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
+                  {post.about1}
+                </a>
+              </p>
+
+              <p className="mb-5 text-xs">
+                <a href={post.about2} target="_blank" className="bg-slate-500 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
+                  {post.about2}
+                </a>
               </p>
 
               <p className="text-zinc-400 text-xs">
                 Upload on {post.date?.toDate().toLocaleString()}
               </p>
 
-              {post.author && (
-                <p className="text-xs text-zinc-500">
-                  by {post.author}
-                </p>
-              )}
             </div>
           ))}
 
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "www.rawpixel.com",
+      "images.rawpixel.com",
+      "firebasestorage.googleapis.com"
+    ],
+  },
 };
 
 export default nextConfig;
